af8fdfc4d9a19ed657f9cbf9eb3ed0c6db1adb17
zza56 <1514326359@qq.com>
2025-09-09 16:59:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-# WeightConvert,py
-weight_str=input("请输入带有符号的重量值")
-if WeightStr[-2:].lower()=='kg';
-1b=eval(WeightStr[0:-2])*2.2046
-print("转换后的重量是{：4f}1b".format(1b))
-elif WeightStr[-2:].lower()=='1b':
-kg=eval(WeightStr[0:-2])/2.2046
-print("转换后的重量是{:.4f}kg".format(kg))
-else:
-print("输入格式错误，请使用'kg'表示千克或'1b'表示磅")
+# WeightConvert.py
+WeightStr = input("请输入带有符号的重量值：")
+if WeightStr[-2:] == 'kg':
+    # 输入的是千克，转换为磅
+    lb = eval(WeightStr[0:-2]) * 2.2046
+    print("转换后的重量是{:.3f}lb".format(lb))
+elif WeightStr[-2:]
